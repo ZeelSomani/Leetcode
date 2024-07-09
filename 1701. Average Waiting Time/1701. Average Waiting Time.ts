4a28a7223c37887f603391a46106829e780988e8
ZeelSomani <164183987+ZeelSomani@users.noreply.github.com>
2024-07-09 15:49:33 +0530

    1701. Average Waiting Time.ts

---
+++ 1701. Average Waiting Time/1701. Average Waiting Time.ts
@@ -0,0 +1,10 @@
+function averageWaitingTime(customers: number[][]): number 
+  {
+    let [tot, t] = [0, 0];
+    for (const [a, b] of customers) 
+    {
+        t = Math.max(t, a) + b;
+        tot += t - a;
+    }
+    return tot / customers.length;
+}
