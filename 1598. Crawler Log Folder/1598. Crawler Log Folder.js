3f3ea70bb5afdbf181cd482c1ddf513ca3e8fb7e
ZeelSomani <164183987+ZeelSomani@users.noreply.github.com>
2024-07-10 09:24:46 +0530

    1598. Crawler Log Folder.js

---
+++ 1598. Crawler Log Folder/1598. Crawler Log Folder.js
@@ -0,0 +1,16 @@
+function minOperations(logs)
+  {
+    let ans = 0;
+    for (const x of logs) 
+    {
+        if (x === '../') 
+        {
+            ans && ans--;
+        } 
+        else if (x !== './') 
+        {
+            ans++;
+        }
+    }
+    return ans;
+}
