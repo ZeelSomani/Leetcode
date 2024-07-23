3a715c212079a633e6897deddc61cc4b84e9a59b
ZeelSomani <164183987+ZeelSomani@users.noreply.github.com>
2024-07-23 06:27:00 +0530

    1636. Sort Array by Increasing Frequency.ts

---
+++ 1636. Sort Array by Increasing Frequency/1636. Sort Array by Increasing Frequency.ts
@@ -0,0 +1,9 @@
+function frequencySort(nums: number[]): number[] 
+{
+    const map = new Map<number, number>();
+    for (const num of nums) 
+    {
+        map.set(num, (map.get(num) ?? 0) + 1);
+    }
+    return nums.sort((a, b) => map.get(a) - map.get(b) || b - a);
+}
