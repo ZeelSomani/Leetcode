0ec85208db4fbd64d50d22330cfee72fbb24dd4d
ZeelSomani <164183987+ZeelSomani@users.noreply.github.com>
2024-07-23 06:27:46 +0530

    1636. Sort Array by Increasing Frequency.js

---
+++ 1636. Sort Array by Increasing Frequency/1636. Sort Array by Increasing Frequency.js
@@ -0,0 +1,10 @@
+var frequencySort = function (nums)
+{
+    const m = new Map();
+    for (let i = 0; i < nums.length; i++) 
+    {
+        m.set(nums[i], (m.get(nums[i]) || 0) + 1);
+    }
+    nums.sort((a, b) => (m.get(a) != m.get(b) ? m.get(a) - m.get(b) : b - a));
+    return nums;
+};
