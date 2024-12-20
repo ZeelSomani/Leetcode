d0f47f39df8ccd265cb62724d066501c72bd538c
ZeelSomani <164183987+ZeelSomani@users.noreply.github.com>
2024-12-20 09:05:48 +0530

    Create 2415.Reverse Odd Levels of Binary Tree.py

---
+++ 2415. Reverse Odd Levels of Binary Tree/2415.Reverse Odd Levels of Binary Tree.py
@@ -0,0 +1,17 @@
+class Solution:
+    def reverseOddLevels(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        q = deque([root])
+        i = 0
+        while q:
+            if i & 1:
+                l, r = 0, len(q) - 1
+                while l < r:
+                    q[l].val, q[r].val = q[r].val, q[l].val
+                    l, r = l + 1, r - 1
+            for _ in range(len(q)):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                    q.append(node.right)
+            i += 1
+        return root
