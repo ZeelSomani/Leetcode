279ccdbae90e67893dffce47709e0f120bbbfd0e
ZeelSomani <164183987+ZeelSomani@users.noreply.github.com>
2024-12-25 08:41:22 +0530

    Create 0515.Find Largest Value in Each Tree Row1.go

---
+++ 0515.Find Largest Value in Each Tree Row/0515.Find Largest Value in Each Tree Row1.go
@@ -0,0 +1,23 @@
+
+func largestValues(root *TreeNode) (ans []int) {
+	if root == nil {
+		return
+	}
+	q := []*TreeNode{root}
+	for len(q) > 0 {
+		x := q[0].Val
+		for i := len(q); i > 0; i-- {
+			node := q[0]
+			q = q[1:]
+			x = max(x, node.Val)
+			if node.Left != nil {
+				q = append(q, node.Left)
+			}
+			if node.Right != nil {
+				q = append(q, node.Right)
+			}
+		}
+		ans = append(ans, x)
+	}
+	return
+}
