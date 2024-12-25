ea26b234dd95583ad4abdbc70eef4d81e09d37c9
ZeelSomani <164183987+ZeelSomani@users.noreply.github.com>
2024-12-25 08:43:06 +0530

    Create 0515.Find Largest Value in Each Tree Row1.java

---
+++ 0515.Find Largest Value in Each Tree Row/0515.Find Largest Value in Each Tree Row1.java
@@ -0,0 +1,26 @@
+
+class Solution {
+    public List<Integer> largestValues(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        if (root == null) {
+            return ans;
+        }
+        Deque<TreeNode> q = new ArrayDeque<>();
+        q.offer(root);
+        while (!q.isEmpty()) {
+            int t = q.peek().val;
+            for (int i = q.size(); i > 0; --i) {
+                TreeNode node = q.poll();
+                t = Math.max(t, node.val);
+                if (node.left != null) {
+                    q.offer(node.left);
+                }
+                if (node.right != null) {
+                    q.offer(node.right);
+                }
+            }
+            ans.add(t);
+        }
+        return ans;
+    }
+}
