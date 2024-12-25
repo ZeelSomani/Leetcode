f7c14c4705f84d82c05108620f96acb083b1001d
ZeelSomani <164183987+ZeelSomani@users.noreply.github.com>
2024-12-25 08:41:59 +0530

    Create 0515.Find Largest Value in Each Tree Row2.go

---
+++ 0515.Find Largest Value in Each Tree Row/0515.Find Largest Value in Each Tree Row2.go
@@ -0,0 +1,19 @@
+
+func largestValues(root *TreeNode) []int {
+	var ans []int
+	var dfs func(*TreeNode, int)
+	dfs = func(root *TreeNode, curr int) {
+		if root == nil {
+			return
+		}
+		if curr == len(ans) {
+			ans = append(ans, root.Val)
+		} else {
+			ans[curr] = max(ans[curr], root.Val)
+		}
+		dfs(root.Left, curr+1)
+		dfs(root.Right, curr+1)
+	}
+	dfs(root, 0)
+	return ans
+}
