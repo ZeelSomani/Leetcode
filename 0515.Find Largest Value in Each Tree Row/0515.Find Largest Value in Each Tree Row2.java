52f0f06b2559db17d52028ca939137378badb809
ZeelSomani <164183987+ZeelSomani@users.noreply.github.com>
2024-12-25 08:43:39 +0530

    Create 0515.Find Largest Value in Each Tree Row2.java

---
+++ 0515.Find Largest Value in Each Tree Row/0515.Find Largest Value in Each Tree Row2.java
@@ -0,0 +1,22 @@
+
+class Solution {
+    private List<Integer> ans = new ArrayList<>();
+
+    public List<Integer> largestValues(TreeNode root) {
+        dfs(root, 0);
+        return ans;
+    }
+
+    private void dfs(TreeNode root, int curr) {
+        if (root == null) {
+            return;
+        }
+        if (curr == ans.size()) {
+            ans.add(root.val);
+        } else {
+            ans.set(curr, Math.max(ans.get(curr), root.val));
+        }
+        dfs(root.left, curr + 1);
+        dfs(root.right, curr + 1);
+    }
+}
