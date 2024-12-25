c14629b8837fd5bb224e2aa04f5bc117fbc1df51
ZeelSomani <164183987+ZeelSomani@users.noreply.github.com>
2024-12-25 08:44:47 +0530

    Create 0515.Find Largest Value in Each Tree Row2.py

---
+++ 0515.Find Largest Value in Each Tree Row/0515.Find Largest Value in Each Tree Row2.py
@@ -0,0 +1,16 @@
+
+class Solution:
+    def largestValues(self, root: Optional[TreeNode]) -> List[int]:
+        def dfs(root, curr):
+            if root is None:
+                return
+            if curr == len(ans):
+                ans.append(root.val)
+            else:
+                ans[curr] = max(ans[curr], root.val)
+            dfs(root.left, curr + 1)
+            dfs(root.right, curr + 1)
+
+        ans = []
+        dfs(root, 0)
+        return ans
