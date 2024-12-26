b4e4b4fd93c17e9ca95b205f55930c491f81e23d
ZeelSomani <164183987+ZeelSomani@users.noreply.github.com>
2024-12-26 11:45:52 +0530

    Create 0002. Add Two Numbers.nim

---
+++ 0002. Add Two Numbers/0002. Add Two Numbers.nim
@@ -0,0 +1,19 @@
+
+proc addTwoNumbers(l1: var SinglyLinkedList, l2: var SinglyLinkedList): SinglyLinkedList[int] =
+  var
+    aggregate: SinglyLinkedList
+    psum: seq[char]
+    temp_la, temp_lb: seq[int]
+
+  while not l1.head.isNil:
+    temp_la.add(l1.head.value)
+    l1.head = l1.head.next
+
+  while not l2.head.isNil:
+    temp_lb.add(l2.head.value)
+    l2.head = l2.head.next
+
+  psum = reversed($(reversed(temp_la).join("").parseInt() + reversed(temp_lb).join("").parseInt()))
+  for i in psum: aggregate.append(($i).parseInt())
+
+  result = aggregate
