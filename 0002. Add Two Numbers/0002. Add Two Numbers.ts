27bc5939460cd0b12d08d066db1885179f5f6184
ZeelSomani <164183987+ZeelSomani@users.noreply.github.com>
2024-12-26 11:49:00 +0530

    Create 0002. Add Two Numbers.ts

---
+++ 0002. Add Two Numbers/0002. Add Two Numbers.ts
@@ -0,0 +1,20 @@
+
+function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
+    const dummy = new ListNode();
+    let cur = dummy;
+    let sum = 0;
+    while (l1 != null || l2 != null || sum !== 0) {
+        if (l1 != null) {
+            sum += l1.val;
+            l1 = l1.next;
+        }
+        if (l2 != null) {
+            sum += l2.val;
+            l2 = l2.next;
+        }
+        cur.next = new ListNode(sum % 10);
+        cur = cur.next;
+        sum = Math.floor(sum / 10);
+    }
+    return dummy.next;
+}
