7edef469b8ddddfe17652923c367b66c1b622860
ZeelSomani <164183987+ZeelSomani@users.noreply.github.com>
2024-12-26 12:03:21 +0530

    Create 0004.Median of Two Sorted Arrays.nim

---
+++ 0004.Median of Two Sorted Arrays/0004.Median of Two Sorted Arrays.nim
@@ -0,0 +1,15 @@
+import std/[algorithm, sequtils]
+
+proc medianOfTwoSortedArrays(nums1: seq[int], nums2: seq[int]): float =
+  var
+    fullList: seq[int] = concat(nums1, nums2)
+    value: int = fullList.len div 2
+
+  fullList.sort()
+
+  if fullList.len mod 2 == 0:
+    result = (fullList[value - 1] + fullList[value]) / 2
+  else:
+    result = fullList[value].toFloat()
+
+
