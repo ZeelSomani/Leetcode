c16e6975d6a6984faf6b5a02cabe45dcf67da6cf
ZeelSomani <164183987+ZeelSomani@users.noreply.github.com>
2024-12-26 12:11:36 +0530

    Create 0007.Reverse Integer.php

---
+++ 0007.Reverse Integer/0007.Reverse Integer.php
@@ -0,0 +1,24 @@
+class Solution {
+ 
+
+    function reverse($x) {
+        $isNegative = $x < 0;
+        $x = abs($x);
+
+        $reversed = 0;
+
+        while ($x > 0) {
+            $reversed = $reversed * 10 + ($x % 10);
+            $x = (int) ($x / 10);
+        }
+
+        if ($isNegative) {
+            $reversed *= -1;
+        }
+        if ($reversed < -pow(2, 31) || $reversed > pow(2, 31) - 1) {
+            return 0;
+        }
+
+        return $reversed;
+    }
+}
