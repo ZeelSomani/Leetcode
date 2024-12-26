00368469b8bb3afc9278f5ab490c75f2211ca455
ZeelSomani <164183987+ZeelSomani@users.noreply.github.com>
2024-12-26 12:31:30 +0530

    Create 0011.Container With Most Water.php

---
+++ 0011.Container With Most Water/0011.Container With Most Water.php
@@ -0,0 +1,18 @@
+class Solution {
+ 
+    function maxArea($height) {
+        $i = 0;
+        $j = count($height) - 1;
+        $ans = 0;
+        while ($i < $j) {
+            $t = min($height[$i], $height[$j]) * ($j - $i);
+            $ans = max($ans, $t);
+            if ($height[$i] < $height[$j]) {
+                ++$i;
+            } else {
+                --$j;
+            }
+        }
+        return $ans;
+    }
+}
