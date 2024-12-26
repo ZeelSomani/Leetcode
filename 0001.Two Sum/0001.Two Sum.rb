c43c8c56533f40480daa21768c9cfb8c39a8d671
ZeelSomani <164183987+ZeelSomani@users.noreply.github.com>
2024-12-26 11:40:14 +0530

    Create 0001.Two Sum.rb

---
+++ 0001.Two Sum/0001.Two Sum.rb
@@ -0,0 +1,11 @@
+
+def two_sum(nums, target)
+    d = {}
+    nums.each_with_index do |x, i|
+      y = target - x
+      if d.key?(y)
+        return [d[y], i]
+      end
+      d[x] = i
+    end
+end
