e7cfe24a52aac84fc0c59f033cb27fb4deb6e7b6
ZeelSomani <164183987+ZeelSomani@users.noreply.github.com>
2025-01-15 19:28:51 +0530

    Create 0022.Generate Parentheses.php

---
+++ 0022.Generate Parentheses/0022.Generate Parentheses.php
@@ -0,0 +1,18 @@
+class Solution {
+function generateParenthesis($n) {
+    $ans = [];
+
+    $dfs = function($l, $r, $t) use ($n, &$ans, &$dfs) {
+        if ($l > $n || $r > $n || $l < $r) return;
+        if ($l == $n && $r == $n) {
+            $ans[] = $t;
+            return;
+        }
+        $dfs($l + 1, $r, $t . "(");
+        $dfs($l, $r + 1, $t . ")");
+    };
+
+    $dfs(0, 0, "");
+    return $ans;
+}
+}
