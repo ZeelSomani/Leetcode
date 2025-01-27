8f714c71e89b81708e572b96fb6b69f9acbdecbf
ZeelSomani <164183987+ZeelSomani@users.noreply.github.com>
2025-01-27 21:46:04 +0530

    Create 0023.Merge k Sorted Lists.cs

---
+++ 0023.Merge k Sorted Lists/0023.Merge k Sorted Lists.cs
@@ -0,0 +1,22 @@
+
+public class Solution {
+    public ListNode MergeKLists(ListNode[] lists) {
+        PriorityQueue<ListNode, int> pq = new PriorityQueue<ListNode, int>();
+        foreach (var head in lists) {
+            if (head != null) {
+                pq.Enqueue(head, head.val);
+            }
+        }
+        var dummy = new ListNode();
+        var cur = dummy;
+        while (pq.Count > 0) {
+            var node = pq.Dequeue();
+            cur.next = node;
+            cur = cur.next;
+            if (node.next != null) {
+                pq.Enqueue(node.next, node.next.val);
+            }
+        }
+        return dummy.next;
+    }
+}
