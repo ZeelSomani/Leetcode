38d5c731629d2422c9f3e96a34067b5c7c1b3bd8
ZeelSomani <164183987+ZeelSomani@users.noreply.github.com>
2025-01-27 21:47:15 +0530

    Create 0023.Merge k Sorted Lists.ts

---
+++ 0023.Merge k Sorted Lists/0023.Merge k Sorted Lists.ts
@@ -0,0 +1,16 @@
+
+function mergeKLists(lists: Array<ListNode | null>): ListNode | null {
+    const pq = new MinPriorityQueue({ priority: (node: ListNode) => node.val });
+    lists.filter(head => head).forEach(head => pq.enqueue(head));
+    const dummy: ListNode = new ListNode();
+    let cur: ListNode = dummy;
+    while (!pq.isEmpty()) {
+        const node = pq.dequeue().element;
+        cur.next = node;
+        cur = cur.next;
+        if (node.next) {
+            pq.enqueue(node.next);
+        }
+    }
+    return dummy.next;
+}
